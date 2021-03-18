135368a83a842bf9872d25ad049cb4a069b06def
Miguel Mesquita <mm@cob.pt>
2021-03-18 15:31:49 +0000

    feat: add rmDefinitionAdvSearch

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const { setServer, getServer } = require("./server")
 const { auth, getUsername } = require("./auth")
 const { rmDefinitionSearch } = require("./rmDefinitionSearch")
+const { rmDefinitionAdvSearch } = require("./rmDefinitionAdvSearch")
 const { rmDomainSearch } = require("./rmDomainSearch")
 const { rmGetInstance } = require("./rmGetInstance")
 const { rmAddInstance } = require("./rmAddInstance")
 const { rmDeleteInstance } = require("./rmDeleteInstance")
 const { umLoggedin } = require("./umLoggedin")
 
-module.exports = { auth, getUsername, setServer, getServer, rmDefinitionSearch, rmDomainSearch, rmGetInstance, rmAddInstance, rmDeleteInstance, umLoggedin };
+module.exports = { auth, getUsername, setServer, getServer, rmDefinitionSearch, rmDefinitionAdvSearch, rmDomainSearch, rmGetInstance, rmAddInstance, rmDeleteInstance, umLoggedin };
