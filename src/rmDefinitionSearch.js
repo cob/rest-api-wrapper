cde80f340bfe9bf2ac3893ad0df9c9c8fb3e87c9
Miguel Mesquita <mm@cob.pt>
2021-03-25 00:20:43 +0000

    refactor: simplify URL calculation

--- src/rmDefinitionSearch.js
+++ src/rmDefinitionSearch.js
@@ -4,7 +4,8 @@ const axios = require('axios');
 const QueryURLTemplate =  "/recordm/recordm/definitions/search/name/__DEF_NAME__?from=__FROM__&size=__SIZE__&q=__QUERY__"
 const ResultsURLTemplate = "/recordm/#/definitions/__DEF_ID__/q=__QUERY__"
 
-var rmDefinitionSearch = async function (definitionName, query="*", from=0, size=0,sort="", ascending="") {
+var rmDefinitionSearch = async function (definitionName, query="*", from=0, size=0, sort="", ascending="") {
+  //TODO: verificar se o AXIOS permite especificar correctamente os query parameters 
   let queryUrl = QueryURLTemplate
     .replace('__DEF_NAME__',definitionName)
     .replace('__QUERY__',query)
@@ -21,12 +22,9 @@ var rmDefinitionSearch = async function (definitionName, query="*", from=0, size
       let defId = def[Object.keys(def)[0]].id
       
       //Add resultsUrl to response
-      response.data.resultsUrl = ResultsURLTemplate
+      response.data.resultsUrl = getServer() + ResultsURLTemplate
         .replace('__DEF_ID__', defId)
-        .replace('__QUERY__', query);
-      if(typeof window == "undefined") {
-        response.data.resultsUrl = getServer() + response.data.resultsUrl
-      }
+        .replace('__QUERY__', query)
 
       return response.data
     })
