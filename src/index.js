fe33b3365b66a44fef31d37ff1dee51290a1298f
Miguel Mesquita <mm@cob.pt>
2021-03-28 13:40:21 +0100

    fix: corrects exports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { setServer, getServer } from "./server"
-import { auth, getUsername } from "./auth"
-import { rmDefinitionSearch } from "./rmDefinitionSearch"
-import { rmDefinitionAggregation } from "./rmDefinitionAggregation"
-import { rmDomainSearch } from "./rmDomainSearch"
-import { rmGetInstance } from "./rmGetInstance"
-import { rmAddInstance } from "./rmAddInstance"
-import { rmDeleteInstance } from "./rmDeleteInstance"
-import { umLoggedin } from "./umLoggedin"
+import auth from "./auth"
+import rmDefinitionSearch from "./rmDefinitionSearch"
+import rmDefinitionAggregation from "./rmDefinitionAggregation"
+import rmDomainSearch from "./rmDomainSearch"
+import rmGetInstance from "./rmGetInstance"
+import rmAddInstance from "./rmAddInstance"
+import rmDeleteInstance from "./rmDeleteInstance"
+import umLoggedin from "./umLoggedin"
 
-export { auth, getUsername, setServer, getServer, rmDefinitionSearch, rmDefinitionAggregation, rmDomainSearch, rmGetInstance, rmAddInstance, rmDeleteInstance, umLoggedin };
+export { auth, setServer, getServer, rmDefinitionSearch, rmDefinitionAggregation, rmDomainSearch, rmGetInstance, rmAddInstance, rmDeleteInstance, umLoggedin };
