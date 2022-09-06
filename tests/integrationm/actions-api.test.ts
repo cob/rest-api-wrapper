f3f1543e39f8f0f9775fb25e6b19ccf2af3cd804
Hugo Marcelino <hugo.m.marcelino@gmail.com>
2022-09-06 11:42:28 +0100

    fix the unit test

--- tests/integrationm/actions-api.test.ts
+++ tests/integrationm/actions-api.test.ts
@@ -1,7 +1,8 @@
 import {ActionsApi} from "@cob/cobjs-integrationm";
 
 test("Can call a concurrent action", async () => {
+    let data = {name: "Guest"};
     const actionsApi = new ActionsApi();
-    const result = await actionsApi.executeConcurrentAction("welcome", {name: "Guest"})
-    expect(result.data).toStrictEqual(`Hey ${{name: "Guest"}.name}. Welcome to Cultofbits!`)
+    const result = await actionsApi.executeConcurrentAction("welcome", data)
+    expect(result.data).toStrictEqual(`Hey ${data.name}. Welcome to Cultofbits!`)
 })
