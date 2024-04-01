58c59a18e2e8f048061d606af9a4e5e16ff3e0b0
dCastanho <dc@cob.pt>
2024-04-01 17:00:04 +0100

    Added timezone to definition search
    Added timezone as optional parameter `tz`. Default value is empty for backwards compatibility. Updated rmDefinitionSearch tests to account for new data in Learning. Added new test to check for new argument. rmDefinitionSearch tests pass.

--- src/rmDefinitionSearch.js
+++ src/rmDefinitionSearch.js
@@ -1,16 +1,17 @@
 import { getServer } from "./server.js";
 import axios from 'axios';
 
-const QueryURLTemplate =  "/recordm/recordm/definitions/search/name/__DEF_NAME__?from=__FROM__&size=__SIZE__&q=__QUERY__"
+const QueryURLTemplate =  "/recordm/recordm/definitions/search/name/__DEF_NAME__?from=__FROM__&size=__SIZE__&q=__QUERY__&tz=__TIMEZONE__"
 const ResultsURLTemplate = "#/definitions/__DEF_ID__/q=__QUERY__"
 
-const rmDefinitionSearch = async function (definitionName, query="*", from=0, size=0, sort="", ascending="") {
+const rmDefinitionSearch = async function (definitionName, query="*", from=0, size=0, sort="", ascending="", tz="") {
   //TODO: verificar se o AXIOS permite especificar correctamente os query parameters 
   let queryUrl = QueryURLTemplate
     .replace('__DEF_NAME__',definitionName)
     .replace('__QUERY__',encodeURIComponent(query))
     .replace('__FROM__',from)
     .replace('__SIZE__',size)
+    .replace('__TIMEZONE__', tz)
 
   if(sort) queryUrl += "&sort="+sort
   if(ascending) queryUrl += "&ascending="+ascending
--- tests/rmDefinitionSearch.test.js
+++ tests/rmDefinitionSearch.test.js
@@ -6,7 +6,7 @@ import rmDefinitionSearch from "../src/rmDefinitionSearch"
 test('for the learning server, "countries series" count for "Arab world" is 20', () => {
     rmDefinitionSearch("Countries Series", "Arab world")
     .then( result => {
-        expect(result.hits.total.value).toBe(20)
+        expect(result.hits.total.value).toBe(22)
     })
 })
 
@@ -37,6 +37,18 @@ test('search for a definition that does not exist logs an error and throws "Defi
 test('for the learning server, "countries series" count for "Arab world" is 20, even when using + signs in the query' , () => {
     rmDefinitionSearch("Countries Series", "Arab world year.date:<now-100y+100y")
     .then( result => {
-        expect(result.hits.total.value).toBe(20)
+        expect(result.hits.total.value).toBe(22)
     })
+})
+
+test('for the learning server, "countries series" count for the date 2018-10-07. With timezone Europe/Lisbon it should find several matches. Without it, it will not find anything.' , async () => {
+    // Dates are stored as UTC at midnight. This means without the Europe/Lisbon 
+    // timezone, they are seen as the previous day during daylight savings 
+    // (00h00 - 1h = 23h00 prev day) and as such the query misses them.
+    const with_tz = await rmDefinitionSearch("Countries Series", "year.date:2018-07-10", 0, 0, "", "", "Europe/Lisbon")
+    expect(with_tz.hits.total.value).toBeGreaterThan(0)
+    
+    const without_tz = await rmDefinitionSearch("Countries Series", "year.date:2018-07-10")
+    expect(without_tz.hits.total.value).toBe(0)
+
 })
